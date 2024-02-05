90caac6b199a758224c8f01d3c55c7b8c3a38f01
Chitrakathi <Vinay.Chitrakathi@bs.nttdata.com>
2024-02-05 21:03:35 +0530

    modified app code

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     if uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
     # Split documents into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=20)
     texts = text_splitter.create_documents(documents)
     # Select embeddings
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
@@ -42,7 +42,7 @@ with st.form('myform', clear_on_submit=True):
     # File upload
     uploaded_file = st.file_uploader('Upload an article', type='txt')
     # Query text
-    query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)   
+    query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.')
 
     # openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and openai_api_key and query_text))
